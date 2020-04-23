2b0639f874a69dccf6c07d5caba9fafa072537d8
ThaNinjaPower <kevinvannguyen1997@gmail.com>
2020-04-23 11:05:25 -0500

    Separated prompts for each employee type and now checks for errors in input

--- app.js
+++ app.js
@@ -12,121 +12,258 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
 var employees = [];
+var usedIds = [];
+var usedEmails = [];
+var usedGithubs = [];
+var managerExists = false;
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-inquirer.prompt([
-    // Manager stuff
-    {
-        type: "input",
-        message: "Enter manager name:",
-        name: "managerName"
-    },
 
-    {
-        type: "number",
-        message: "Enter manager's ID:",
-        name: "managerID"
-    },
+console.log("Kevin's Team Builder App");
+console.log();
+console.log("Build an engineering team consisting of one manager and multiple engineers and interns!");
+console.log();
+mainPrompt();
 
-    {
-        type: "input",
-        message: "Enter manager's email:",
-        name: "managerEmail"
-    },
+function mainPrompt() {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "Choose employee type:",
+            name: "employeeChoice",
+            choices: [
+                "Manager",
+                "Engineer",
+                "Intern",
+                "Finished building team"
+            ]
+        }
+    ]).then(function (response) {
+        switch(response.employeeChoice) {
+            case "Manager":
+                if (managerExists) {
+                    console.log("Manager already exists! Choose engineer or intern.");
+                    console.log();
+                    mainPrompt();
+                }
+                else {
+                    console.log();
+                    managerExists = true;
+                    managerPrompt();
+                }
+                break;
 
-    {
-        type: "number",
-        message: "Enter manager's room number:",
-        name: "managerRoomNumber"
-    },
+            case "Engineer":
+                console.log();
+                engineerPrompt();
+                break;
 
-    // Engineer stuff
-    {
-        type: "input",
-        message: "Enter an engineer's name:",
-        name: "engineerName",
-    },
+            case "Intern":
+                console.log();
+                internPrompt();
+                break;
 
-    {
-        type: "number",
-        message: "Enter engineer's ID:",
-        name: "engineerID"
-    },
+            default:
+                console.log();
+                renderPage();
+        }
+    });
+}
 
-    {
-        type: "input",
-        message: "Enter engineer's github username:",
-        name: "engineerUserName"
-    },
+function managerPrompt() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Enter manager name:",
+            name: "managerName"
+        },
 
-    {
-        type: "input",
-        message: "Enter engineer's email:",
-        name: "engineerEmail"
-    },
+        {
+            type: "number",
+            message: "Enter manager's ID:",
+            name: "managerId"
+        },
 
-    // Intern stuff
-    {
-        type: "input",
-        message: "Enter intern's name:",
-        name: "internName"
-    },
+        {
+            type: "input",
+            message: "Enter manager's email:",
+            name: "managerEmail"
+        },
 
-    {
-        type: "number",
-        message: "Enter intern's ID:",
-        name: "internID"
-    },
+        {
+            type: "number",
+            message: "Enter manager's room number:",
+            name: "managerRoomNumber"
+        }
+    ]).then(function (response) {
+        if (!checkId(response.managerId) || !checkEmail(response.managerEmail) || !checkRoomNumber(response.managerRoomNumber)) {
+            console.log();
+            managerPrompt();
+        }
 
-    {
-        type: "input",
-        message: "Enter intern's email:",
-        name: "internEmail"
-    },
+        else {
+            const teamManager = new Manager(response.managerName, response.managerId, response.managerEmail, response.managerRoomNumber);
+            
+            pushUniqueInfo(teamManager, response.managerId, response.managerEmail);
+            
+            console.log();
+            mainPrompt();
+        }
+    });
+}
 
-    {
-        type: "input",
-        message: "Enter intern's school:",
-        name: "internSchool"
+function engineerPrompt() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Enter an engineer's name:",
+            name: "engineerName",
+        },
+
+        {
+            type: "number",
+            message: "Enter engineer's ID:",
+            name: "engineerId"
+        },
+
+        {
+            type: "input",
+            message: "Enter engineer's email:",
+            name: "engineerEmail"
+        },
+
+        {
+            type: "input",
+            message: "Enter engineer's github username:",
+            name: "engineerGithub"
+        }
+    ]).then(function (response) {
+        if (!checkId(response.engineerId) || !checkEmail(response.engineerEmail) || !checkGithub(response.engineerGithub)) {
+            console.log();
+            engineerPrompt();
+        }
+
+        else {
+            var teamEngineer = new Engineer(response.engineerName, response.engineerId, response.engineerEmail, response.engineerGithub);
+            
+            usedGithubs.push(response.engineerGithub);
+            pushUniqueInfo(teamEngineer, response.engineerId, response.engineerEmail);
+            
+            console.log();
+            mainPrompt();
+        }
+    });
+}
+
+function internPrompt() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Enter intern's name:",
+            name: "internName"
+        },
+
+        {
+            type: "number",
+            message: "Enter intern's ID:",
+            name: "internId"
+        },
+
+        {
+            type: "input",
+            message: "Enter intern's email:",
+            name: "internEmail"
+        },
+
+        {
+            type: "input",
+            message: "Enter intern's school:",
+            name: "internSchool"
+        }
+    ]).then(function(response) {
+        if (!checkId(response.internId) || !checkEmail(response.internEmail)) {
+            console.log();
+            internPrompt();
+        }
+
+        else {
+            var teamIntern = new Intern(response.internName, response.internId, response.internEmail, response.internSchool);
+            
+            pushUniqueInfo(teamIntern, response.internId, response.internEmail);
+            
+            console.log();
+            mainPrompt();
+        }
+    });
+}
+
+function checkId(inputId) {
+    console.log();
+
+    if (usedIds.includes(inputId)) {
+        console.log("ID is already used! Try again and enter a unique ID.");
+        console.log("IDs used: " + usedIds);
+        return false;
     }
+    else if (inputId === undefined || inputId <= 0) {
+        console.log("ID is not a valid number! Try again and enter a positive integer for the ID.");
+    }
+    else {
+        return true;
+    }
+}
 
-]).then(response => {
-    const teamManager = new Manager(response.managerName, response.managerID, response.managerEmail, response.managerRoomNumber);
-    employees.push(teamManager);
-    const teamEngineer = new Engineer(response.engineerName, response.engineerID, response.engineerEmail, response.engineerUserName);
-    employees.push(teamEngineer);
-    const teamIntern = new Intern(response.internName, response.internID, response.internEmail, response.internSchool);
-    employees.push(teamIntern);
+function checkEmail(inputEmail) {
+    if (usedEmails.includes(inputEmail)) {
+        console.log();
+        console.log("Email already exists in this team! Try again and enter a unique email.");
+        console.log("Emails used: " + usedEmails);
+        return false;
+    }
+    else {
+        return true;
+    }
+}
 
-    console.log(employees);
+function checkRoomNumber(inputRoomNumber) {
+    if (inputRoomNumber === undefined || inputRoomNumber <= 0) {
+        console.log("Room number is not a valid number! Try again and enter a positive integer for the room number.");
+        return false;
+    }
+    else {
+        return true;
+    }
+}
 
-    fs.writeFileSync("./output/index.html", render(employees), function(error) {
-        if(error) {
+function checkGithub(inputGithub) {
+    if(usedGithubs.includes(inputGithub)) {
+        console.log();
+        console.log("Github username already exists in this team! Try again and enter a unique Github username.");
+        console.log("Github usernames used: " + usedGithubs);
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+function pushUniqueInfo(employee, inputId, inputEmail) {
+    employees.push(employee);
+    usedIds.push(inputId);
+    usedEmails.push(inputEmail);
+}
+
+function renderPage() {
+    console.log();
+    console.log("Full team: " + employees);
+    console.log();
+    fs.writeFileSync(outputPath, render(employees), function (error) {
+        if (error) {
             console.log(error);
         }
         else {
-            console.log("Success!");
+            console.log("Team page generated in './output/index.html' !");
         }
     });
-});
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+}
